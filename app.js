441772145cae9235c71ce4102cbb2191c1bc67ce
AJavier55 <aliciajavier5@gmail.com>
2021-08-26 19:55:32 -0400

    mobile menu made in js and styled

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const navLogo = document.querySelector('#navbar__logo');
 const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector('#mobile-menu');
+
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+  };
+  
+  menu.addEventListener('click', mobileMenu);
